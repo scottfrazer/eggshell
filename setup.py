c1b04d170852c9dc5f6e656abe66f79f9992f539
Scott Frazer <scott.d.frazer@gmail.com>
2014-08-12 13:22:14 -0400

    renaming to eggshell

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 version = '1.0'
-long_description = 'Conch is a shell interface'
+long_description = 'Eggshell is a shell interface'
 
 setup(
-  name='conch',
+  name='eggshell',
   version=version,
   description=long_description,
   author='Scott Frazer',
   author_email='scott.d.frazer@gmail.com',
-  packages=['conch'],
-  package_dir={'conch': 'conch'},
+  packages=['eggshell'],
+  package_dir={'eggshell': 'eggshell'},
   license = 'MIT',
   keywords = "Bash, Shell",
-  url = "http://github.com/scottfrazer/conch",
+  url = "http://github.com/scottfrazer/eggshell",
   classifiers=[
       'License :: OSI Approved :: MIT License',
       "Programming Language :: Python",
